867a2bc3afe9217dadd0d203657c41d884681f4b
Seoyoung Jeong <issyo1022@gmail.com>
2022-02-11 02:21:53 +0900

    feat: ChatListItem 컴포넌트 추가

--- src/components/organisms/ChatRoom/style.ts
+++ src/components/organisms/ChatRoom/style.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   flex-grow: 1;
-  background-color: #f9fafc;
+  background-color: #f8fafb;
   border: 1px solid #e6e6e6;
   border-radius: 20px;
   padding: 20px;
