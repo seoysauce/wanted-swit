ac39bfc80d1803eda375647a1b1ce60cc84ae2e0
Seoyoung Jeong <issyo1022@gmail.com>
2022-02-11 17:47:02 +0900

    feat: currentChatRoom 데이터 추가, 아이콘 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,4 +2,5 @@ import defaultUser from 'assets/images/defaultUser.png';
 
 export { Logo } from 'assets/icons/Logo';
 export { Magnifier } from 'assets/icons/Magnifier';
+export { Person } from 'assets/icons/Person';
 export { defaultUser };
---
+++ src/commons/currentChatRoom.ts
@@ -0,0 +1,9 @@
+export const CURRENT_CHAT_ROOM = {
+  title: 'Team Tyranno',
+  membersCount: 4,
+  imageUrl: 'https://art.pixilart.com/d8a5d6f1f1f432a.png',
+  unread: 0,
+  lastMessage: '',
+  lastTimestamp: '',
+  status: 'online',
+};
